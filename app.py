f2d0049a5e7146468f3d2feebc7eb8a3146f174b
Yogesh Singh <yogeshsingh1599@gmail.com>
2026-04-14 01:15:28 +0530

    setup ci cd pipeline

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def home():
-    return "DevOps App Running inside Docker!"
+    return "DevOps App is Running inside Docker!"
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
